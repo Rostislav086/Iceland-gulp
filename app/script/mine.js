3cbd6f899dec1cc44b25812c117d7a27e13cff55
Rostislav <Rostislav.086@mail.ru>
2020-10-15 21:23:00 +0500

    added swiper route

--- app/script/mine.js
+++ app/script/mine.js
@@ -7,19 +7,18 @@ var mySwiper = new Swiper('.swiper-container', {
 	  speed: 4000,
 
 	// Navigation arrows
-	navigation: {
-	  nextEl: '.swiper-button-next',
-	  prevEl: '.swiper-button-prev',
-	},
   });
 
   mySwiper.init();
 
   let routeSwiper = new Swiper('.swiper-route', {
-	loop: true,
-	// autoplay: {
-	// 	delay: 2000,
-	//   },
+	loop: false,
+	speed: 2000,
+	hideOnClick: true,
+	navigation: {
+		nextEl: '.route-button-next',
+		prevEl: '.route-button-prev',
+	  },
   });
 
   routeSwiper.init();
